1ed8d9ec841bafed8a95a5c30ebb80115efa97df
Ruben Franco <ruben.afranco@yahoo.com>
2023-03-15 19:25:33 -0700

    created roles table

--- schema.sql
+++ schema.sql
@@ -11,3 +11,12 @@ CREATE TABLE department (
   name VARCHAR(30)
  
 );
+
+-- Role Table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
