d9138f7e89cae6384f18bfa16856487efe7d6171
Ruben Franco <ruben.afranco@yahoo.com>
2023-03-16 22:28:16 -0700

    added mysql2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 
 const inquirer = require("inquirer")
-const mysql = require("mysql")
+const mysql = require("mysql2")
 const cTable = require('console.table');
 
 const connection = mysql.createConnection({
